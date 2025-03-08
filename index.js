cbce7cf5e15915a6cbfe1b77a65219b08640eaad
Abdullahi <abdullahiibrashair@gmail.com>
2025-03-08 12:45:34 +0300

    phase-1-arrow-functions

---
+++ index.js
@@ -0,0 +1,15 @@
+// write your code here
+const divide=function(){
+    let sum=2000/100
+    return sum
+}
+const square=()=>{
+    let square=2*2
+    return square
+}
+const add=()=>{
+    let param1=3
+    let param2=4
+    let add =(param1+param2)
+    return add
+}
